2499c10d3840c637d94a767c3a7da0a310599514
Johannes Becker <johannes.becker@4soft.de>
2025-10-13 08:06:56 +0200

    Fixed printable generation.

--- wiki/generate-pdf.js
+++ wiki/generate-pdf.js
@@ -15,8 +15,8 @@ function createDirectories(pathname) {
 }
 
 const pdfPages = [
-    { url: 'http://localhost:8080/specifications/vec/guidelines/index.html', outputPath: './pdf/specifications/vec/guidelines/vec-guidelines.pdf' },
-    { url: 'http://localhost:8080/specifications/kbl/guidelines/index.html', outputPath: './pdf/specifications/kbl/guidelines/kbl-guidelines.pdf' },
+    { url: 'http://localhost:8080/specifications/vec/guidelines/printable.html', outputPath: './pdf/specifications/vec/guidelines/vec-guidelines.pdf' },
+    { url: 'http://localhost:8080/specifications/kbl/guidelines/printable.html', outputPath: './pdf/specifications/kbl/guidelines/kbl-guidelines.pdf' },
     // Add more pages as needed
 ];
 
