4a2d7c3032ca3b4b9424a5c66594b457fdea4e58
Johannes Becker <johannes.becker@4soft.de>
2026-01-29 12:44:47 +0100

    Deactivated timeout.

--- wiki/generate-pdf.js
+++ wiki/generate-pdf.js
@@ -25,7 +25,7 @@ const pdfPages = [
 
     // Launch a headless browser
     const browser = await puppeteer.launch({
-        timeout: 60000,
+        timeout: 0,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox'
